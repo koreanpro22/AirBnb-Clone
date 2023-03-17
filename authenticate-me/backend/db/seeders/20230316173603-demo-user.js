9eb64799562b41fd5f3291c40eb30d9300b87827
David Kim <dhskim22@gmail.com>
2023-03-16 20:51:14 -0400

    first dev merge

--- authenticate-me/backend/db/seeders/20230316173603-demo-user.js
+++ authenticate-me/backend/db/seeders/20230316173603-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        fistName: 'A',
+        lastName: 'aA',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'B',
+        lastName: 'bB',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'C',
+        lastName: 'cC',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
